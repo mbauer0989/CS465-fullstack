4e1a3f5e547310ecbc340311106df38d600f48ef
B-Dub ! <mbauer0989@gmail.com>
2021-09-06 16:15:39 -0400

    Revert "Created Angular SPA, created logic for Angular SPA to retrieve data from backend, created add trip and edit trip logic"
    This reverts commit 363134aad2b494ff362a603f6c9660a1d7adb2bd.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,13 +5,10 @@ const tripsController = require ('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList)
-    .post(tripsController.tripsAddTrip);
-
+    .get(tripsController.tripsList);
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindCode)
-    .put(tripsController.tripsUpdateTrip);
+    .get(tripsController.tripsFindCode);
     
 module.exports = router;
